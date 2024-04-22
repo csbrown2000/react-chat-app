05602ce940b98d26446a755e597d1f55b5cba341
Collin Brown <collinbrown@Collins-MacBook-Pro.local>
2024-04-22 14:01:11 -0600

    Updating CORS

--- backend/main.py
+++ backend/main.py
@@ -23,7 +23,8 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173",
+                   "https://main.d1zagmzalu4759.amplifyapp.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
